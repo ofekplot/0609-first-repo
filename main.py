f89bfd6d049ff389c964c1602e10c0def73e4d92
ofekLotem <greatplot@gmail.com>
2023-12-15 09:47:02 +0200

    Changed first value to 123

--- main.py
+++ main.py
@@ -14,7 +14,7 @@ def print_hi(name):
 # Press the green button in the gutter to run the script.
 if __name__ == '__main__':
     # print_hi('World')
-    first = 75
+    first = 123
     second = "33"
     # result = first + second
     print("Result is:" , first, second)
